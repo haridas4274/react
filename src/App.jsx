cff9969b95a3442daec421d0343471134178480b
haridasezeeinfo <thedonharidas@gmail.com>
2024-06-09 01:33:18 +0530

    useEffect init

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
+import React from "react"
+import { useState } from "react"
+import { useEffect } from "react"
 
+//useEffect Hooks
+
+//syntax 
+// useEffect(function,[])
 function App() {
 
+  const[count,setCount] = useState(0);
+
+  useEffect(()=>{
+    setInterval(()=>{
+      setCount(count + 1);
+    },2000);
+  })
 
   return (
-    <div>
-   Hello world
-    </div>
+  <>
+  <h1>I have Rendered with {count} times!</h1>
+  </>
   )
 }
 
